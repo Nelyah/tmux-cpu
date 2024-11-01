e0573e062319fedd6ec793e7fbe1cbde6d209d95
Nelyah <contact@nelyah.eu>
2024-11-01 12:55:47 +0100

    Only show actively used memory usage using vm_stat

--- scripts/ram_percentage.sh
+++ scripts/ram_percentage.sh
@@ -6,10 +6,11 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/helpers.sh"
 
 ram_percentage_format="%3.1f%%"
+pagesize="$(pagesize)"
 
 sum_macos_vm_stats() {
   grep -Eo '[0-9]+' |
-    awk '{ a += $1 * 4096 } END { print a }'
+    awk '{ a += $1 * '"$pagesize"' } END { print a }'
 }
 
 print_ram_percentage() {
@@ -20,22 +21,25 @@ print_ram_percentage() {
   elif command_exists "vm_stat"; then
     # page size of 4096 bytes
     stats="$(cached_eval vm_stat)"
+    free_fields='Pages free'
+    active_use_fields='Pages active|Pages wired down'
+    cached_fields='Pages purgeable|File-backed pages|Pages occupied by compressor|Pages speculative'
 
     used_and_cached=$(
       echo "$stats" |
-        grep -E "(Pages active|Pages inactive|Pages speculative|Pages wired down|Pages occupied by compressor)" |
+        grep -E "(${active_use_fields}|${cached_fields})" |
         sum_macos_vm_stats
     )
 
     cached=$(
       echo "$stats" |
-        grep -E "(Pages purgeable|File-backed pages)" |
+        grep -E "(${cached_fields})" |
         sum_macos_vm_stats
     )
 
     free=$(
       echo "$stats" |
-        grep -E "(Pages free)" |
+        grep -E "(${free_fields})" |
         sum_macos_vm_stats
     )
 
